ed407274f375839cb06e0577478e0175d3019bbc
Guillaume Blin <lemail2guillaume@gmail.com>
2020-01-18 10:52:24 +0100

    Create vpl_img2standalone.sh

---
+++ misc/scripts/vpl_img2standalone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Transforming a vpl configured docker image into a standalone one without the vpl capability but the same configuration as the evaluation environment.
+# Guillaume Blin and Corentin Abel Mercier - guillaume.blin@u-bordeaux.fr ; corentin-abel.mercier@etu.u-bordeaux.fr
+# copyright 2020 Guillaume Blin - Corentin Abel Mercier
+# license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+rm /usr/bin/mygit /usr/bin/git /usr/bin/mysvn /usr/bin/svn
+mv /usr/bin/gitorig /usr/bin/git
+mv /usr/bin/svnorig /usr/bin/svn
+rm -f /vplbdx/*
